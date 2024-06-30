699c2583835aa1e904e3c9f83efed264c4244cb4
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-06-30 10:26:46 +0300

    Update Routes

--- MidEndBlogs/src/App.jsx
+++ MidEndBlogs/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./features/HomePage";
 import Signup from "./features/Signup";
 import Login from "./features/Login";
+import WelcomePage from "./features/WelcomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route index element={<WelcomePage />} />
+        <Route path="home" element={<HomePage />}/>
         <Route path="signup" element={<Signup />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </BrowserRouter>
   );
