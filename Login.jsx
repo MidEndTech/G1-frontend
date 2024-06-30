96abeb1bcb6b992aea691d5ac32e0b7d797fcabf
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-06-30 08:25:44 +0300

    Creating welcome page + some modification on login & signup pages

--- Login.jsx
+++ Login.jsx
@@ -1,14 +1,56 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/Input";
 import { useState } from "react";
+import Cookies from "js-cookie";
+const host = import.meta.env.SERVER_HOST;
+const port = import.meta.env.SERVER_PORT;
 
-function login() {
+function Login() {
 
+  const [error, setError] = useState("");
+  const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
+  const user = {
+    email: String,
+    password: String,
+  };
 
-  const fetchAPI = async () => {};
+  const fetchAPI = async () => {
+    const res = await fetch(`http://${host}:${port}/api/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (res.status === 200){
+      const data = await res.json();
+        const token = data.token;
+
+        Cookies.set("token", token, { expires: 2 });
+        navigate("/home");
+    } 
+    else if(res.status === 401){
+      setIsError(true);
+      setError("Invalid email or password. Please try again");
+    }
+    
+    else{
+      setIsError(true);
+        setError("Something went wrong. Please try again");
+    };
+
+  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (email && password) {
+      user.email = e.target.email.value;
+      user.password = e.target.password.value;
+    }
 
     await fetchAPI();
   };
@@ -41,14 +83,16 @@ function login() {
             <button className="p-2 bg-primary text-center text-white mt-8 rounded-full shadow-lg">
               LOG IN
             </button>
-            <Link to="/login" className="font-semibold text-center mt-4">
-              AlREADY HAVE AN ACCOUNT?
+            <Link to="/signup" className="font-semibold text-center mt-4">
+              YOU DON'T HAVE AN ACCOUNT? SIGN UP!
             </Link>
+
           </form>
         </section>
       </div>
     </div>
   );
 }
+
 
 export default Login;
