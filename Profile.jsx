f853f4fe36be6ce53fcb1a9535c07903af498b62
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-07-01 09:39:48 +0300

    Added sign out button Without the popup page //workin on it (:

--- Profile.jsx
+++ Profile.jsx
@@ -1,65 +1,76 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Profile(){
+function Profile() {
+  const [records, setRecords] = useState([]);
 
-    const [records, setRecords] = useState([])
+  /*useEffect(() => {
+      fetch('url')
+      .then(res => res.json())
+      .then(data => setRecords(data))
+      .catch(err => console.log(err))
+  },[])*/
 
-    {/*useEffect(() => {
-        fetch('url')
-        .then(res => res.json())
-        .then(data => setRecords(data))
-        .catch(err => console.log(err))
-    },[])*/}
+  return (
+    <>
+    
+    <div className="bg-neutral-100 flex flex-col items-center gap-8 p-8">
 
+      <div className="flex flex-col items-center">
+        <img
+          className="w-32 h-32 md:w-48 md:h-48 rounded-full object-cover"
+          src="https://via.placeholder.com/150"
+          alt="Profile Image"/>
+        <p className="text-lg font-bold mt-4">User's Name Variable</p>
+      </div>
 
-    return(
-        <div className="bg-neutral-100">
-        
-            <div className="justify-center flex w-80">
-            <img className="w-16 justify-center flex
-            min-w-96
-            lg:w-96
-            mt-14-
-            
-            mx-auto
-            rounded-full"
-            src="https://www.searchenginejournal.com/wp-content/uploads/2020/08/7-ways-a-blog-can-help-your-business-right-now-5f3c06b9eb24e-1024x538.png"/>
-            <br /> Name
-            </div>
-            
-            <hr className="mt-5 border-2"/>
+      <hr className="border-2 w-full" />
 
-            <div className="mt-10 justify-center flex">
-                Profile <br />
-                <section className="border-solid border-2 border-gray-300 w-96 mt-10 p-3">
-                  <p className="p-1">Name:</p>
-                  <p className="p-1">UserName:</p>
-                  <p className="p-1">Email:</p>
+      <div className="flex flex-col items-center">
+        <p className="text-lg font-bold">Profile details</p>
+        <section className="border-2 border-gray-300 mt-4 p-4 min-w-64">
+          <p className="mb-2">
+            <span className="font-semibold">Name:</span> {}
+          </p>
+          <p className="mb-2">
+            <span className="font-semibold">Username:</span> {}
+          </p>
+          <p className="mb-2">
+            <span className="font-semibold">Email:</span> {}
+          </p>
+        </section>
+      </div>
 
+      <div className="flex flex-col items-center">
+        <p className="text-lg font-bold">Posts</p>
+        <section className="border-2 border-gray-300 mt-4 p-4 min-w-70">
+          <p className="mb-2">1</p>
+          <p className="mb-2">2</p>
+          <p className="mb-2">3</p>
+        </section>
+      </div>
+
+        <div>
+          <section className="bg-emerald-300 bg-opacity-25 p-4">
+              <p className="font-semibold justify-center flex">Time to go?</p>
+                <section className="border-2 rounded-lg border-opacity-85 rad border-gray-500 mt-3 px-5 py-1 ">
+                    <button>SIGN OUT</button>
                 </section>
-            </div>
-
-
-            <div className="mt-10 justify-center flex">
-                Posts
-                <section className="border-solid border-2 border-gray-300 w-9/12 mt-10 p-3 pb">
-                    <p className="p-1">1</p>
-                    <p className="p-1">2</p>
-                    <p className="p-1"></p>
-                </section>
-            </div>
-
-
-        {/*<ul>
-        {records.map((list, index) => (
-            <li key={index }>{list.id} | {list.id}</li>
-        )
-    )}
-        </ul>*/}
-
+          </section>
         </div>
-    );
-};
+
+        <div>DELETE ACOOUNT</div>
+
+      {/*<ul>
+        {records.map((list, index) => (
+          <li key={index}>{list.id} | {list.id}</li>
+        ))}
+      </ul>*/}
+    
+    </div>
+  
+  </>
+  );
+}
 
 export default Profile;
