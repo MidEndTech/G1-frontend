6b903281a5ba36fdd852840fb71c6e32814c4ec6
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-07-03 14:53:03 +0300

    Update Leaderboard.jsx

--- MidEndBlogs/src/features/Leaderboard.jsx
+++ MidEndBlogs/src/features/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 
 const url = import.meta.env.VITE_SERVER_URL;
